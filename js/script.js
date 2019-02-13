714f2e8dbe0b62068e52719014dc12f05773eaae
Tom <tomn98@live.unc.edu>
2019-02-13 14:53:21 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,31 +1,33 @@
-var x;
-var y;
-var z;
+$(function() {
+    var x;
+    var y;
+    var z;
 
-function degToRad( degrees ) {
-    var pi = Math.PI;
-    return degrees * (pi/180);
-};
+    function degToRad( degrees ) {
+        var pi = Math.PI;
+        return degrees * (pi/180);
+    };
 
-function rotateNeedle(needle, degrees) {
-    if (degrees == NaN) {
-        degrees = 0;
+    function rotateNeedle(needle, degrees) {
+        if (degrees == NaN) {
+            degrees = 0;
+        }
+        needle.css("transform", "rotate(" + degrees + "deg) translateX(75px);")
     }
-    needle.css("transform", "rotate(" + degrees + "deg) translateX(75px);")
-}
 
-function motion(event){
-    x = event.beta;
-    y = event.gamma;
-    z = event.alpha;
-    
-    $("#x").text("X: " + x);
-    $("#y").text("Y: " + y);
-    $("#z").text("Z: " + z);
-    
-    rotateNeedle($("#ui-needle-x"), x);
-    rotateNeedle($("#ui-needle-y"), y);
-    rotateNeedle($("#ui-needle-z"), z);
-}
+    function motion(event){
+        x = event.beta;
+        y = event.gamma;
+        z = event.alpha;
 
-window.addEventListener("deviceorientation", motion);
+        $("#x").text("X: " + x);
+        $("#y").text("Y: " + y);
+        $("#z").text("Z: " + z);
+
+        rotateNeedle($("#ui-needle-x"), x);
+        rotateNeedle($("#ui-needle-y"), y);
+        rotateNeedle($("#ui-needle-z"), z);
+    }
+
+    window.addEventListener("deviceorientation", motion);
+})
